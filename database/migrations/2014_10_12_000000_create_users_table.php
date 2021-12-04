1f62288b19525e6787fe7e600c47692fb19a3b86
G.A.JAGUAR <dev.gajaguar@gmail.com>
2021-12-04 13:30:34 -0600

    :recycle: refactor(migrations): improve `users` migrations
    Add enable | disable foreign key constraint to
    avoid conflicts during unordered migrations,
    separate unique constraint sentence and limit
    `string` fields size.

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,15 +13,19 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
+        Schema::disableForeignKeyConstraints();
         Schema::create('users', function (Blueprint $table) {
             $table->uuid('id');
-            $table->string('name');
-            $table->string('email')->unique();
+            $table->string('name', 100);
+            $table->string('email', 50);
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->string('password', 50);
             $table->rememberToken();
             $table->timestamps();
+
+            $table->unique('email');
         });
+        Schema::enableForeignKeyConstraints();
     }
 
     /**
@@ -31,6 +35,8 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
+        Schema::disableForeignKeyConstraints();
         Schema::dropIfExists('users');
+        Schema::enableForeignKeyConstraints();
     }
 }
