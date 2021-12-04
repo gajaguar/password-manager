e953245f764be9685929814738da3925142638e1
G.A.JAGUAR <dev.gajaguar@gmail.com>
2021-12-04 14:46:28 -0600

    :construction: chore(models): set `category` model relationships

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -2,7 +2,9 @@
 
 namespace App\Models;
 
+use App\Models\Record;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Str;
 
@@ -22,8 +24,44 @@ class Category extends Model
      */
     protected $keyType = 'string';
 
+    /**
+     * Indicates if the IDs are auto-incrementing.
+     */
     public $incrementing = false;
 
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var string[]
+     */
+    protected $fillable = [
+        'category_name',
+        'category_description',
+    ];
+
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'created_at',
+    ];
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'updated_at' => 'datetime',
+    ];
+
+    /**
+     * The 'booting' method of the model.
+     *
+     * @return void
+     */
     protected static function boot()
     {
         parent::boot();
@@ -31,5 +69,15 @@ class Category extends Model
         static::creating(function (User $user) {
             $user->setAttribute($user->getKeyName(), Str::uuid()->toString());
         });
+    }
+
+    /**
+     * Set relationship with the records.
+     *
+     * @return HasMany
+     */
+    public function records()
+    {
+        return $this->hasMany(Record::class);
     }
 }
