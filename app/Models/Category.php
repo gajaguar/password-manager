b3f99220fff2c47fcf8c98de95cace38e33cb3d1
G.A.JAGUAR <dev.gajaguar@gmail.com>
2021-12-04 14:30:10 -0600

    :construction: chore(models): set `user` model relationships

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -6,10 +6,20 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Str;
 
+/**
+ * @property string id
+ * @property string category_name
+ * @property string category_description
+ * @property mixed created_at
+ * @property mixed updated_at
+ */
 class Category extends Model
 {
     use HasFactory;
 
+    /**
+     * Chanhe the default primary key type to a string.
+     */
     protected $keyType = 'string';
 
     public $incrementing = false;
--- app/Models/User.php
+++ app/Models/User.php
@@ -2,20 +2,43 @@
 
 namespace App\Models;
 
+use App\Models\Record;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Support\Str;
 use Laravel\Sanctum\HasApiTokens;
 
+/**
+ * @property string id
+ * @property string name
+ * @property string email
+ * @property mixed email_verified_at
+ * @property string password
+ * @property string remember_token
+ * @property mixed created_at
+ * @property mixed updated_at
+ */
 class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
 
+    /**
+     * Change the default primary key type to a string.
+     *
+     * @var string
+     */
     protected $keyType = 'string';
 
+    /**
+     * Indicates if the IDs are auto-incrementing.
+     *
+     * @var bool
+     */
     public $incrementing = false;
+
     /**
      * The attributes that are mass assignable.
      *
@@ -46,6 +69,11 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
     ];
 
+    /**
+     * The 'booting' method of the model.
+     *
+     * @return void
+     */
     protected static function boot()
     {
         parent::boot();
@@ -53,5 +81,15 @@ class User extends Authenticatable
         static::creating(function (User $user) {
             $user->setAttribute($user->getKeyName(), Str::uuid()->toString());
         });
+    }
+
+    /**
+     * Set relationship with the records.
+     *
+     * @return HasMany
+     */
+    public function records()
+    {
+        return $this->hasMany(Record::class);
     }
 }
