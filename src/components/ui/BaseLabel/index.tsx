6c365dedbc451c4f98b0c1cb16baf6af0271679a
G.A.JAGUAR <dev@gajaguar.com>
2023-03-02 18:47:55 -0600

    ✨ feat(components-ui): create base label

---
+++ src/components/ui/BaseLabel/index.tsx
@@ -0,0 +1,19 @@
+import { ReactNode } from "react"
+
+type BaseLabelProps = {
+  children?: ReactNode
+  htmlFor?: string
+}
+
+function BaseLabel({
+  children,
+  htmlFor,
+}: BaseLabelProps) {
+  return (
+    <label htmlFor={htmlFor}>
+      {children}
+    </label>
+  )
+}
+
+export default BaseLabel
